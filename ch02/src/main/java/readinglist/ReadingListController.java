5a3f597e093612aa4bf25f1a53c9a3a68a0c4159
ctllin <guolinit@163.com>
2018-12-09 20:26:38 +0800

    init

--- ch02/src/main/java/readinglist/ReadingListController.java
+++ ch02/src/main/java/readinglist/ReadingListController.java
@@ -2,6 +2,8 @@ package readinglist;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ReadingListController {
 
   private static final String reader = "craig";
-  
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	private ReadingListRepository readingListRepository;
 
 	@Autowired
@@ -23,7 +26,7 @@ public class ReadingListController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String readersBooks(Model model) {
-		
+		logger.info("reading list ....");
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
@@ -33,8 +36,10 @@ public class ReadingListController {
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public String addToReadingList(Book book) {
+		logger.info("reading add ....{}",book);
 		book.setReader(reader);
 		readingListRepository.save(book);
+
 		return "redirect:/readingList";
 	}
 	
